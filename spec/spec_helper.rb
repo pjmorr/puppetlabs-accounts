2e51bb8126818ffe43ea08c213cef6ea62dcf644
Philip Morrison <phil@uberops.com>
2016-07-12 20:42:14 -0500

    GEORGE-69 excluded ci gem vendor path from CI rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/spec/'
+  # Exclude bundled Gems in `/.vendor/`
+  add_filter '/vendor/'
+end
